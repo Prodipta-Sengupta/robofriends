1b035b8eb51dbf5339e084e9f01d264e2ae8864a
Prodipta Sengupta <47535510+Prodipta-Sengupta@users.noreply.github.com>
2019-10-13 20:21:22 +0530

    Updates

---
+++ precache-manifest.4b6e8f89ace272607b7b78a27fb94fb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73975215a51b787fc77e14122727417",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "bba29b52a5ec9ad62584",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "eead3c5fa8b9ff13ba08",
+    "url": "/robofriends/static/css/main.f03ab79d.chunk.css"
+  },
+  {
+    "revision": "bba29b52a5ec9ad62584",
+    "url": "/robofriends/static/js/2.1954d486.chunk.js"
+  },
+  {
+    "revision": "eead3c5fa8b9ff13ba08",
+    "url": "/robofriends/static/js/main.35a6b647.chunk.js"
+  },
+  {
+    "revision": "1eafa76e58301bf2aef6",
+    "url": "/robofriends/static/js/runtime~main.b45798a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robofriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
